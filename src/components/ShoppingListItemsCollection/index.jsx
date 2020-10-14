8986fbb01325e9464d2f651b5035b6713159b418
Marina Marunevskaya <marina.marunevskaya@gmail.com>
2020-10-14 16:35:55 +0300

    Reselect was added for total price

--- src/components/ShoppingListItemsCollection/index.jsx
+++ src/components/ShoppingListItemsCollection/index.jsx
@@ -1,27 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 
 import { ShoppingListItem } from '../ShoppingListItem';
 
 const defaultShoppingListItemsCollection = ({
 	listId,
-	items
+	items,
+	totalPrice
 }) => {
 	if (items.length) {
 		return (
-			<ul className="list-group">
-				{
-					items.map(
-						({ id, name, price }) => <ShoppingListItem
-							id={id}
-							key={id}
-							listId={listId}
-							name={name}
-							price={price}
-						/>
-					)
-				}
-			</ul>
+			<>
+				<ul className="list-group">
+					{
+						items.map(
+							({ id, name, price }) => <ShoppingListItem
+								id={id}
+								key={id}
+								listId={listId}
+								name={name}
+								price={price}
+							/>
+						)
+					}
+					<li key={`total-${listId}`} className="list-group-item list-group-item-info">
+						<div className="row">
+							<div className="col font-weight-bold">
+								Total price
+							</div>
+							<div className="col text-md-right text-sm-left">
+								{totalPrice}
+							</div>
+						</div>
+					</li>
+				</ul>
+			</>
 		);
 	} else {
 		return (
@@ -32,8 +46,22 @@ const defaultShoppingListItemsCollection = ({
 	}
 };
 
-const mapStateToProps = (store, props) => ({
-	items: store.items.items[props.listId]
-});
+const getListItems = (store, props) => store.items.items[props.listId];
+const generateCalculateTotalPrice = () => {
+	return createSelector(
+		getListItems,
+		items => items.reduce((total, item) => total + item.price, 0)
+	);
+};
 
-export const ShoppingListItemsCollection = connect(mapStateToProps)(defaultShoppingListItemsCollection);
+const generateMapStateToProps = () => {
+	const calculateTotalPrice = generateCalculateTotalPrice();
+	const mapStateToProps = (store, props) => ({
+		items: getListItems(store, props),
+		totalPrice: calculateTotalPrice(store, props)
+	});
+
+	return mapStateToProps;
+};
+
+export const ShoppingListItemsCollection = connect(generateMapStateToProps)(defaultShoppingListItemsCollection);
