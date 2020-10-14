6ed9da02e324e34770f8020276cc1c6bfd946b8e
Marina Marunevskaya <marina.marunevskaya@gmail.com>
2020-10-14 11:45:28 +0300

    ShoppingListsCollection

---
+++ src/components/ShoppingListsCollection/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { ShoppingList } from '../ShoppingList';
+
+const defaultShoppingListsCollection = ({
+	items
+}) => {
+	if (items.length) {
+		return (
+			<ul className="list-group">
+				{
+					items.map(
+						({ id, name }) => <ShoppingList id={id} key={id} name={name} />
+					)
+				}
+			</ul>
+		);
+	} else {
+		return (
+			<p className="alert alert-warning" role="alert">
+				No shopping lists...
+			</p>
+		);
+	}
+};
+
+const mapStateToProps = store => ({
+	items: store.lists
+});
+
+export const ShoppingListsCollection = connect(mapStateToProps)(defaultShoppingListsCollection);
