92f6852583822402a9e7eb0de7f120d6778b59a1
Marina Marunevskaya <marina.marunevskaya@gmail.com>
2020-10-14 11:45:41 +0300

    App

---
+++ src/App/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { ShoppingListsCollection } from '../components/ShoppingListsCollection';
+
+export const App = () => (
+	<div className="container mt-5">
+		<ShoppingListsCollection />
+	</div>
+);
