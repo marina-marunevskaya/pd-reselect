59476c410460af8a925e36b578622e85d2335566
Marina Marunevskaya <marina.marunevskaya@gmail.com>
2020-10-14 13:55:27 +0300

    listReducer logic was updated

--- src/actions/itemActions.js
+++ src/actions/itemActions.js
@@ -1,7 +1,17 @@
+export const ITEM_LIST_INIT = 'ITEM_LIST_INIT';
 export const ITEM_ADD = 'ITEM_ADD';
 export const ITEM_UPDATE = 'ITEM_UPDATE';
 export const ITEM_DELETE = 'ITEM_DELETE';
 export const ITEM_CLEAR_LIST_ITEMS = 'ITEM_CLEAR_LIST_ITEMS';
+
+export const initListItems = (
+	listId
+) => ({
+	type: ITEM_LIST_INIT,
+	payload: {
+		listId
+	}
+});
 
 export const addItem = (
 	id,
@@ -20,23 +30,27 @@ export const addItem = (
 
 export const updateItem = (
 	id,
+	listId,
 	name,
 	price
 ) => ({
 	type: ITEM_UPDATE,
 	payload: {
 		id,
+		listId,
 		name,
 		price
 	}
 });
 
 export const deleteItem = (
-	id
+	id,
+	listId
 ) => ({
 	type: ITEM_DELETE,
 	payload: {
-		id
+		id,
+		listId
 	}
 });
 
@@ -47,4 +61,4 @@ export const clearListItems = (
 	payload: {
 		listId
 	}
-})
+});
--- src/reducers/itemReducer.js
+++ src/reducers/itemReducer.js
@@ -1,4 +1,5 @@
 import {
+	ITEM_LIST_INIT,
 	ITEM_ADD,
 	ITEM_UPDATE,
 	ITEM_DELETE,
@@ -8,33 +9,62 @@ import {
 import { createReducer } from './createReducer';
 
 const initialState = {
-	items: []
+	items: {}
 };
 
 export const itemReducer = createReducer(
 	initialState,
 	{
-		[ITEM_ADD]: (state, newItem) => ({
+		[ITEM_LIST_INIT]: (state, { listId }) => ({
 			...state,
-			items: [...state.items, newItem]
+			items: {
+				...state.items,
+				[listId]: []
+			}
 		}),
-		[ITEM_UPDATE]: (state, updatedItem) => ({
+		[ITEM_ADD]: (state, { listId, ...newItem }) => ({
 			...state,
-			items: state.items.map(
-				item => (
-					item.id === updatedItem.id
-						? updatedItem
-						: item
+			items: {
+				...state.items,
+				[listId]: [
+					...state.items[listId],
+					newItem
+				]
+			}
+		}),
+		[ITEM_UPDATE]: (state, { listId, ...updatedItem }) => ({
+			...state,
+			items: {
+				...state.items,
+				[listId]: state.items[listId].map(
+					item => (
+						item.id === updatedItem.id
+							? updatedItem
+							: item
+					)
 				)
-			)
+			}
 		}),
-		[ITEM_DELETE]: (state, { id }) => ({
+		[ITEM_DELETE]: (state, { id, listId }) => ({
 			...state,
-			items: state.items.filter(item => item.id !== id)
+			items: {
+				...state.items,
+				[listId]: state.items[listId].filter(item => item.id !== id)
+			}
 		}),
-		[ITEM_CLEAR_LIST_ITEMS]: (state, { listId }) => ({
-			...state,
-			items: state.items.filter(item => item.listId !== listId)
-		})
+		[ITEM_CLEAR_LIST_ITEMS]: (state, { listId }) => {
+			let items = {};
+
+			for (let list in state.items) {
+				if (list !== listId) {
+					items[list] = state.items[list];
+				}
+			}
+
+			return {
+				...state,
+				items
+			};
+		}
 	}
 );
