9ba09bec5226e75328e21ab94b240085657ec99c
Marina Marunevskaya <marina.marunevskaya@gmail.com>
2020-10-15 13:46:16 +0300

    itemReducer was updated

--- src/reducers/itemReducer.js
+++ src/reducers/itemReducer.js
@@ -1,9 +1,6 @@
 import {
 	ITEM_LIST_INIT,
-	ITEM_ADD,
-	ITEM_UPDATE,
-	ITEM_DELETE,
-	ITEM_CLEAR_LIST_ITEMS
+	ITEM_ADD
 } from '../actions/itemActions';
 
 import { createReducer } from './createReducer';
@@ -15,7 +12,7 @@ const initialState = {
 export const itemReducer = createReducer(
 	initialState,
 	{
-		[ITEM_LIST_INIT]: (state, { listId }) => ({
+		[ITEM_LIST_INIT]: (state, listId) => ({
 			...state,
 			items: {
 				...state.items,
@@ -31,40 +28,6 @@ export const itemReducer = createReducer(
 					newItem
 				]
 			}
-		}),
-		[ITEM_UPDATE]: (state, { listId, ...updatedItem }) => ({
-			...state,
-			items: {
-				...state.items,
-				[listId]: state.items[listId].map(
-					item => (
-						item.id === updatedItem.id
-							? updatedItem
-							: item
-					)
-				)
-			}
-		}),
-		[ITEM_DELETE]: (state, { id, listId }) => ({
-			...state,
-			items: {
-				...state.items,
-				[listId]: state.items[listId].filter(item => item.id !== id)
-			}
-		}),
-		[ITEM_CLEAR_LIST_ITEMS]: (state, { listId }) => {
-			let items = {};
-
-			for (let list in state.items) {
-				if (list !== listId) {
-					items[list] = state.items[list];
-				}
-			}
-
-			return {
-				...state,
-				items
-			};
-		}
+		})
 	}
 );
